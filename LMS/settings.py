79ae4699d2eb9e62d975bfc73922104f64b99cd0
Folorunso Timson <110051396+Timozino@users.noreply.github.com>
2023-10-06 09:26:19 +0100

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -157,7 +157,7 @@ SITE_ID = 1
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_root')]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
